b7749ac3b5e448087083d101aa9cf35c0f4b32db
arolen9 <arolen9@users.noreply.github.com>
2015-02-28 21:30:17 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-function comparePositions(arr1, arr2) {
-	var count = 0;
 
-	for (var i = 0; i < arr1.length; i++ )
-	{
-		//console.log ("arr: " + arr + ", count: " + count + ", elem: " + elem);
-
-		if (arr1[i] == arr2[i])
-		{
-			
-			count++;
-			
-		}
-	}
-	return count;
-
-}
